f33071f270b4ba020be2b466b88eced086771c88
KatieAnthon <katieanthonisz@hotmail.co.uk>
2024-04-02 15:25:10 +0100

    added post request for sign up page

--- TheMoneyTree/frontend/pages/SignUp.js
+++ TheMoneyTree/frontend/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, Component} from 'react'
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { useForm, Controller, useFormState } from 'react-hook-form';
 import 'tailwindcss/tailwind.css';
@@ -6,25 +6,61 @@ import 'tailwindcss/tailwind.css';
 
 
 const SignUpScreen = ({navigation}) => {
-  const {control, handleSubmit} = useForm();
-  const {userData, applyUserData} = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
 
-  const onSubmit = (event) => {
-    console.log(event)
-  };
+  const handleSubmit = async (event) => {
+    const userData = {
+      "name": name,
+      "email": email,
+      "password": password
+    }
+
+    fetch('http://127.0.0.1:8080/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData)
+      }
+    )
+
+    .then((response) => response.json())
+    .then((response) => {
+        alert(response[0].message)
+      })
+      .catch((error) => {
+        alert("Error" + error)
+      })
+    }
+    
+
+  
 
 
   return (
     <View className="flex items-center justify-center h-full bg-purple">
     <Text className="text-l mb-4 text-white" >Sign Up here!</Text>
-    <TextInput placeholder="Enter your name" className="border border-white p-2 rounded-md mb-2 w-3/4 text-lg"/>
-    <TextInput placeholder="Enter your email" className="border border-white p-2 rounded-md mb-2 w-3/4 text-lg"/>
     <TextInput 
+    type="name"
+    placeholder={"Enter your name"}
+    onChangeText={text => setName(text)}
+    className="border border-white p-2 rounded-md mb-2 w-3/4 text-lg"
+    />
+    <TextInput 
+    type="email"
+    placeholder="Enter your email" 
+    onChangeText={text => setEmail(email)}
+    className="border border-white p-2 rounded-md mb-2 w-3/4 text-lg"/>
+    <TextInput 
+    type="password"
     placeholder="Enter your password"
+    onChangeText={text => setPassword(password)}
     secureTextEntry={true} 
     className="border border-white p-2 rounded-md mb-2 w-3/4 text-lg justify-centre"/>
-    <Button className="bg-dark text-white font-bold py-8 px-5 rounded" title="Submit" onPress={handleSubmit(onSubmit)}/>
+    <Button className="bg-dark text-white font-bold py-8 px-5 rounded" title="Submit" onPress={handleSubmit}/>
 
     <Button
         title="Log In"
