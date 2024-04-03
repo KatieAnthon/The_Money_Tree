f11923fd9b5a3daa535756601e4385c3ddf5dcee
KatieAnthon <katieanthonisz@hotmail.co.uk>
2024-04-03 17:36:17 +0100

    backend server connected to frontend - successfully adding new users to mongodb database

--- TheMoneyTree/frontend/pages/SignUp.js
+++ TheMoneyTree/frontend/pages/SignUp.js
@@ -16,48 +16,51 @@ const SignUpScreen = ({navigation}) => {
       "name": name,
       "email": email,
       "password": password
-    }
 
-    fetch('http://127.0.0.1:8080/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData)
-      }
-    )
-
-    .then((response) => response.json())
-    .then((response) => {
-        alert(response[0].message)
-      })
-      .catch((error) => {
-        alert("Error" + error)
-      })
     }
+    userString = JSON.stringify(userData)
     
+    try {
+      const response = await fetch("http://192.168.0.102:8080/users/signup", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: userString
+      });
 
+      if (response.ok) {
+        console.log("Successfully added your details!")
+        return "Successfully added your details!"
+      }
+
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+
+      const jsonResponse = await response.json();
+      alert(jsonResponse.message);
+    } catch (error) {
+      alert('Error: ' + error.message);
+    }
+  };
   
-
-
   return (
     <View className="flex items-center justify-center h-full bg-purple">
     <Text className="text-l mb-4 text-white" >Sign Up here!</Text>
     <TextInput 
-    type="name"
     placeholder={"Enter your name"}
     onChangeText={text => setName(text)}
     className="border border-white p-2 rounded-md mb-2 w-3/4 text-lg"
     />
     <TextInput 
-    type="email"
     placeholder="Enter your email" 
-    onChangeText={text => setEmail(email)}
+    onChangeText={text => setEmail(text)}
     className="border border-white p-2 rounded-md mb-2 w-3/4 text-lg"/>
     <TextInput 
-    type="password"
     placeholder="Enter your password"
-    onChangeText={text => setPassword(password)}
+    onChangeText={text => setPassword(text)}
     secureTextEntry={true} 
     className="border border-white p-2 rounded-md mb-2 w-3/4 text-lg justify-centre"/>
     <Button className="bg-dark text-white font-bold py-8 px-5 rounded" title="Submit" onPress={handleSubmit}/>
