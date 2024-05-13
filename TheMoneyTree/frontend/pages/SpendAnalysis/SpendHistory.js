2f69d04f129287ed0fd5a8fb12ecafd05adb1413
KatieAnthon <katieanthonisz@hotmail.co.uk>
2024-05-13 15:52:18 +0100

    added spend history page

---
+++ TheMoneyTree/frontend/pages/SpendAnalysis/SpendHistory.js
@@ -0,0 +1,18 @@
+import React, { useState, Component} from 'react'
+import { Button, StyleSheet, Text, TextInput, View, Pressable } from 'react-native';
+import { useNavigate } from '@react-navigation/native';
+import 'tailwindcss/tailwind.css';
+import { StatusBar } from 'expo-status-bar';
+import "../../global.css"
+
+const SpendHistory = ({navigation}) => {
+
+
+return (
+    <View className="flex items-center justify-center h-full bg-purple-default">
+    <Text className="text-l mb-5 text-white" >Login here</Text>
+    </View>
+    );
+}
+
+export default SpendHistory;
